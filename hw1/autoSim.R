e24a660e826d8636cb386ff565439b8692089834
chenyu1997 <chenyu1997@ucla.edu>
2020-01-24 18:07:53 +0000

    modify runSim

--- hw1/autoSim.R
+++ hw1/autoSim.R
@@ -1,9 +1,20 @@
 # autoSim.R
 
-nVals <- seq(100, 1000, by=100)
-for (n in nVals) {
-  oFile <- paste("n", n, ".txt", sep="")
-  sysCall <- paste("nohup Rscript runSim.R n=", n, " > ", oFile, sep="")
-  system(sysCall, wait = FALSE)
-  print(paste("sysCall=", sysCall, sep=""))
+## parsing command arguments
+for (arg in commandArgs(TRUE)) {
+  eval(parse(text=arg))
+}
+
+# call R
+nVals <- seq(100, 500, by=100)
+distTypes = c("gaussian", "t1", "t5")
+for (dist in distTypes){
+  for (n in nVals) {
+    oFile <- paste("n", n,"dist",dist, ".txt", sep="")
+    arg = paste("n=", n, " dist=", shQuote(shQuote(dist)),
+                " seed=", seed, " rep=", rep)
+    sysCall = paste("nohup Rscript runSim.R ", arg, " > ", oFile, sep="")
+    system(sysCall, wait = FALSE)
+    print(paste("sysCall=", sysCall, sep=""))
+  }
 }
